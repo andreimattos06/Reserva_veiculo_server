4980334a149b7a0fed4ae5324154daa52f711229
Andrei Mattos <andreimattos06@gmail.com>
2023-09-30 21:54:23 -0300

    profile update routes completed

--- app.js
+++ app.js
@@ -125,10 +125,89 @@ app.post('/updatedadosusers', async (request, response) => {
     return response.status(401)
 })
 
-app.post('/updatedadosveiculo', async (request, response) => {
+app.post('/updatedados', async (request, response) => {
+    const body = request.body
+    if (body.id && body.email) {
+        const get_email = await prisma.user.findFirst({
+            where: {
+                email: body.email,
+                id: {
+                    not: body.id
+                }
+            },
+            select: {
+                email: true,
+            }
+        })
+
+        if (get_email === null) {
+
+            const updateUser = await prisma.user.update({
+                where: {
+                    id: Number(body.id)
+                },
+                data: {
+                    email: body.email,
+                    nome_completo: body.nome_completo,
+                    cpf: body.cpf,
+                    administrador: body.administrador,
+                    cargo: body.cargo,
+                    setor: body.setor
+                }
+
+            })
+            return response.json("sucesso")
+        }
+        else {
+            return response.json("Já existe um outro usuário com esse e mail.")
+        }
+
+    }
+    return response.json("Houve um erro.")
+
+})
+
+app.post('/updatesenha', async (request, response) => {
     const body = request.body
     console.log(body)
-    if (body.id) {
+    if (body.senhaantiga && body.senhanova && body.id) {
+        const get_user = await prisma.user.findFirst({
+            where: {
+                id: Number(body.id),
+                senha: md5(body.senhaantiga)
+            },
+            select: {
+                id: true,
+            }
+        })
+        
+        if (get_user != null) {
+            const update_senha = await prisma.user.update({
+                where: {
+                    id: Number(body.id),
+                    senha: md5(body.senhaantiga)
+                },
+                data: {
+                    senha: md5(body.senhanova)
+                }
+            })
+            return response.json("sucesso")
+        }
+        else{
+            return response.json("Senha inválida.")
+        }
+
+    }
+    else {
+        return response.json("Erro.")
+    }
+
+})
+
+
+app.post('/updatedadosveiculo', async (request, response) => {
+    const body = request.body
+      if (body.id) {
         const updateUser = await prisma.carro.update({
             where: {
                 id: Number(body.id)
@@ -161,6 +240,28 @@ app.post('/getdadosusers', async (request, response) => {
                 administrador: true,
                 cargo: true,
                 setor: true
+            }
+
+        })
+        return response.json(result)
+    }
+    return response.status(401)
+})
+
+app.post('/getdados', async (request, response) => {
+    const body = request.body
+    if (body.email) {
+        const result = await prisma.user.findFirst({
+            where: {
+                email: body.email
+            },
+            select: {
+                email: true,
+                nome_completo: true,
+                cpf: true,
+                cargo: true,
+                setor: true,
+                id: true,
             }
 
         })
@@ -258,9 +359,9 @@ app.post('/addveiculo', async (request, response) => {
 
 app.post('/deleteveiculo', async (request, response) => {
     const body = request.body
-    if (body.id){
+    if (body.id) {
         const result = await prisma.carro.delete({
-            where:{
+            where: {
                 id: Number(body.id)
             }
         })
@@ -271,9 +372,9 @@ app.post('/deleteveiculo', async (request, response) => {
 
 app.post('/deleteuser', async (request, response) => {
     const body = request.body
-    if (body.id){
+    if (body.id) {
         const result = await prisma.user.delete({
-            where:{
+            where: {
                 id: Number(body.id)
             }
         })
